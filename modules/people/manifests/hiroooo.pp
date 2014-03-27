5c6320103ada7cc4eb0cd6258d04033b5f34a9fa
鈴木 浩章 <suzukihiroaki@suzuki-no-MacBook-Air.local>
2014-03-27 12:43:21 +0900

    add emacs

--- modules/people/manifests/hiroooo.pp
+++ modules/people/manifests/hiroooo.pp
@@ -1,6 +1,7 @@
 class people::hiroooo {
   include macvim_kaoriya
   include iterm2::stable
+  include emacs
 
   $home = "/Users/{$::boxen_user}"
   $dotfiles = "${home}/.dotfiles"
@@ -9,10 +10,4 @@ class people::hiroooo {
     ensure  => directory
   }
 
-}
-
-package {
-  'GoogleJapaneseInput':
-    source => "http://dl.google.com/japanese-ime/latest/GoogleJapaneseInput.dmg",
-    provider => pkgdmg;
 }
