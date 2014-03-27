6cb7b3dcc4d05229b32a62656fffc636cc081a09
鈴木 浩章 <suzukihiroaki@suzuki-no-MacBook-Air.local>
2014-03-27 12:58:59 +0900

    test

--- modules/people/manifests/hiroooo.pp
+++ modules/people/manifests/hiroooo.pp
@@ -3,6 +3,15 @@ class people::hiroooo {
   include iterm2::stable
   include skype
 
+  package {
+    'XtraFinder':
+      source   => "http://www.trankynam.com/xtrafinder/downloads/XtraFinder.dmg",
+      provider => pkgdmg;
+    'GoogleJapaneseInput':
+      source => "http://dl.google.com/japanese-ime/latest/GoogleJapaneseInput.dmg",
+      provider => pkgdmg;
+  }
+
   $home = "/Users/{$::boxen_user}"
   $dotfiles = "${home}/.dotfiles"
 
